d24d03bf1ab47efc25284452b4507a59f096a275
tenidiotzinternetudefault@gmail.com <>
2023-07-01 22:01:59 +0200

    Drone1 now gets its MovementPattern from IMovementPattern script component

--- Assets/Scripts/EnemyScripts/Drone1.cs
+++ Assets/Scripts/EnemyScripts/Drone1.cs
@@ -5,33 +5,31 @@ using UnityEngine;
 
 public class Drone1 : MonoBehaviour
 {
-    public float MovementSpeed;
-    
-    [SerializeField]
-    public IMovementPattern MovementPattern;
-    public float Rotation;
+    public float rotation;
 
-    public float SpawnTime;
-    public float ShotCooldown;
+    public float spawnTime;
+    public float shotCooldown;
 
     [SerializeField]
     private GameObject _projectile;
     private float _cooldownTimer = 0;
     private bool _isActive = false;
+    
+    private IMovementPattern _movementPattern;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _movementPattern = GetComponent<IMovementPattern>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.timeSinceLevelLoad >= SpawnTime) _isActive = true;
+        if (Time.timeSinceLevelLoad >= spawnTime) _isActive = true;
         if (!_isActive) return;
         
-        transform.position = MovementPattern.GetNewPosition(transform.position);
+        transform.position = _movementPattern.GetNewPosition(transform.position);
         
     }
 }
