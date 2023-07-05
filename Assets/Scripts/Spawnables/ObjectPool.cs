3aa4359e71903e18c802cb0c893624ee47f22bf9
tenidiotzinternetudefault@gmail.com <>
2023-07-05 15:32:24 +0200

    Added Despawn method. Moved setActive method calls from ObjectPool to ObjectPoolManager for better clarity.

--- Assets/Scripts/Spawnables/ObjectPool.cs
+++ Assets/Scripts/Spawnables/ObjectPool.cs
@@ -8,25 +8,21 @@ public class ObjectPool
 {
     public GameObject prefab;
     public int initialPoolSize;
-
+    public int maxPoolSize;
+    
     public int poolSize => pool.Count;
     
     private Queue<GameObject> pool = new();
     
-    public void Enqueue(GameObject obj)
-    {
-        obj.SetActive(false);
-        pool.Enqueue(obj);
-    }
+    public void Enqueue(GameObject obj) => pool.Enqueue(obj);
     
     public GameObject Extract()
     {
         GameObject obj;
-        
+
         obj = pool.Count == 0 ?
             Object.Instantiate(prefab) : pool.Dequeue();
         
-        obj.SetActive(true);
         return obj;
     }
 }
--- Assets/Scripts/Spawnables/ObjectPoolManager.cs
+++ Assets/Scripts/Spawnables/ObjectPoolManager.cs
@@ -30,11 +30,21 @@ public class ObjectPoolManager : MonoBehaviour
         GameObject obj = _poolTable[prefab.name].Extract();
         obj.transform.position = position;
         obj.transform.rotation = rotation;
+        
+        obj.SetActive(true);
         return obj;
     }
 
     public static GameObject Spawn(GameObject prefab, Vector3 position, float rotation)
     {
         return Spawn(prefab, position, Quaternion.Euler(0, 0, rotation));
+    }
+
+    public static GameObject Despawn(GameObject obj)
+    {
+        obj.SetActive(false);
+        _poolTable[obj.name].Enqueue(obj);
+        
+        return obj;
     }
 }
