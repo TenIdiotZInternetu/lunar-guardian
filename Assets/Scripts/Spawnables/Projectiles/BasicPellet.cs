c2d4a85ea3ffc0dbaa0041a9f0ab179a84ae0e26
tenidiotzinternetudefault@gmail.com <>
2023-07-05 14:18:28 +0200

    Generelized PlayerBullet, so it is more flexible with spawners

--- Assets/Scripts/Spawnables/Projectiles/BasicPellet.cs
+++ Assets/Scripts/Spawnables/Projectiles/BasicPellet.cs
@@ -5,15 +5,12 @@ using UnityEngine;
 
 namespace Projectiles
 {
-    public class PlayerBullet1 : MonoBehaviour, IPlayerProjectile
+    public class BasicPellet : MonoBehaviour
     {
         public float movementSpeed;
         public Vector3 direction;
         public float cooldown;
-        public int damage;
-        public List<GameObject> spawners;
-
-        private float _cooldownTimer = 0;
+        
         private IMovementPattern _movementPattern;
         
 
@@ -27,19 +24,6 @@ namespace Projectiles
         void Update()
         {
             transform.position = _movementPattern.GetNewPosition(transform.position);
-            _cooldownTimer -= Time.deltaTime;
-        }
-        
-        public void OnPlayerShoots(object sender, EventArgs e)
-        {
-            if (_cooldownTimer >= 0) return;
-
-            foreach (var spawner in spawners)
-            {
-                Instantiate(this, spawner.transform.position, Quaternion.identity);
-            }
-            
-            _cooldownTimer = cooldown;
         }
     }
 }
--- Assets/Scripts/Spawnables/Projectiles/IPlayerProjectile.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Projectiles
-{
-    public interface IPlayerProjectile
-    {
-        public void OnPlayerShoots(object sender, EventArgs e);
-    }
-}
