273d1e279c27296497ca2bcebea5262b6a6ff297
tenidiotzinternetudefault@gmail.com <>
2023-07-05 15:01:26 +0200

    Generalized BasicPellet even further.

--- Assets/Scripts/Spawnables/Projectiles/BasicPellet.cs
+++ Assets/Scripts/Spawnables/Projectiles/BasicPellet.cs
@@ -7,15 +7,11 @@ namespace Projectiles
 {
     public class BasicPellet : MonoBehaviour
     {
-        public float movementSpeed;
-        public Vector3 direction;
-        public float cooldown;
-        
+        public float Damage;
         private IMovementPattern _movementPattern;
-        
 
         // Start is called before the first frame update
-        void Start()
+        void OnEnable()
         {
             _movementPattern = GetComponent<IMovementPattern>();
         }
