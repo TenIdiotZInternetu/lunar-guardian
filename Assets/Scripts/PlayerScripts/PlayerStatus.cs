fcaa83430640c33f178d1c3e366994fa99e7c520
tenidiotzinternetudefault@gmail.com <>
2023-07-20 20:48:45 +0200

    Added UI with PowerLevel gauge

--- Assets/Scripts/PlayerScripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerScripts/PlayerStatus.cs
@@ -6,12 +6,17 @@ namespace PlayerScripts
     {
         private static int _maxHealth = 5;
         private static int _health = 3;
+        
         private static int _maxBombsHeld = 5;
         private static int _bombsHeld = 2;
+
+        private static int _maxPowerLevel = 200;
+        private static int _powerLevel = 0;
         private static int _score = 0;
         
         public static event EventHandler<int> HealthChangedEvent;
         public static event EventHandler<int> BombsChangedEvent;
+        public static event EventHandler<int> PowerLevelChangedEvent;
         public static event EventHandler<int> ScoreChangedEvent;
         
         public static event EventHandler GameOverEvent;
@@ -31,6 +36,13 @@ namespace PlayerScripts
             _bombsHeld += amount;
             Math.Clamp(_bombsHeld, 0, _maxBombsHeld);
             BombsChangedEvent?.Invoke(null, _bombsHeld);
+        }
+        
+        public static void ChangePowerLevel(int amount)
+        {
+            _powerLevel += amount;
+            Math.Clamp(_powerLevel, 0, _maxPowerLevel);
+            PowerLevelChangedEvent?.Invoke(null, _powerLevel);
         }
         
         public static void IncreaseScore(int amount)
