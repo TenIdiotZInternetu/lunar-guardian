17181c45d875c2fe5fb684320af4ef0adb880572
tenidiotzinternetudefault@gmail.com <>
2023-08-04 14:42:57 +0200

    Decomposed Chasing Movement Pattern for further experimenting (also broke Unity hihi)

--- Assets/Scripts/MovementPatterns/ChasingMP.cs
+++ Assets/Scripts/MovementPatterns/ChasingMP.cs
@@ -1,3 +1,4 @@
+using System;
 using Spawnables;
 using UnityEngine;
 
@@ -7,11 +8,17 @@ namespace MovementPatterns
     {
         [SerializeField] private GameObject target;
         [SerializeField] private float attractionRate;
-        [SerializeField] private float chaseTime;
         [SerializeField] private float initialDirection;
+
+        [SerializeField] private float lockOnTime;
+        [SerializeField] private float chaseTime;
         
         [SerializeField] private float speed;
         [SerializeField] private float acceleration;
+        
+        [SerializeField] private float simulationTimeStep = 0.1f;
+
+        private Vector3 _initialMomentum;
         
         public override Vector3 GetNextPosition(Entity entity)
         {
@@ -21,33 +28,53 @@ namespace MovementPatterns
             
             if (entity.LifeTime > chaseTime)
             {
-                return currentPosition + entity.momentum * currentSpeed;
-            }
-            
-            Vector3 localMomentum = entity.momentum;
-            
-            if (entity.LifeTime < 0.5f)
-            {
-                Vector3 direction = Quaternion.Euler(0, 0, initialDirection) * Vector3.up;
-                localMomentum = direction.normalized * currentSpeed;
+                return currentPosition + entity.momentum;
             }
 
-            Vector3 guideLine = targetPosition - currentPosition;
-            Vector3 expectedPosition = currentPosition + guideLine.normalized * currentSpeed;
-            Vector3 momentumContinuation = currentPosition + localMomentum * currentSpeed;
+            Vector3 localMomentum = (entity.LifeTime < lockOnTime) ?
+                _initialMomentum : 
+                entity.momentum;
             
-            Vector3 finalPosition = Vector3.Lerp(momentumContinuation, expectedPosition, attractionRate);
-            return finalPosition;
+            return Interpolate(currentPosition, targetPosition, localMomentum);
         }
-        
-        private void OnDrawGizmosSelected()
+
+        private Vector3 Interpolate(Vector3 position, Vector3 target, Vector3 momentum)
+        {
+            Vector3 guideLine = target - position;
+            Vector3 newExpectedPosition = position + guideLine.normalized * speed;
+            Vector3 newLinearPosition = position + momentum;
+            
+            return Vector3.Lerp(newLinearPosition, newExpectedPosition, attractionRate / 100);
+        }
+
+        private void OnValidate()
+        {
+            Vector3 direction = Quaternion.Euler(0, 0, initialDirection) * Vector3.up;
+            _initialMomentum = direction.normalized * speed;
+        }
+
+        private void OnDrawGizmos()
         {
             Gizmos.color = Color.green;
             
             Vector3 newPosition = transform.position;
             Vector3 lastPosition = newPosition;
+            Vector3 momentum = _initialMomentum;
+
+            float timeFrame = 10;
+            float timeStep = simulationTimeStep;
             
-            
+            for (float time = 0; time < timeFrame; time += timeStep)
+            {
+                newPosition = (time > chaseTime) ?
+                    lastPosition + momentum :
+                    Interpolate(newPosition, target.transform.position, momentum);
+                
+                Gizmos.DrawLine(lastPosition, newPosition);
+                
+                if (time > lockOnTime) momentum = newPosition - lastPosition;
+                lastPosition = newPosition;
+            }
         }
     }
 }
